dcac86c12e19df805af3e8905a91ba885216afe9
CadeMaynard <143521268+CadeMaynard@users.noreply.github.com>
2023-10-25 21:58:26 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,33 @@
 function dijkstra(graph, sourceNode) {
-    return [];
+    dist = new Array(graph.length);
+    for(i = 0; i < dist.length; i++) {
+        dist[i] = new Array(Infinity, -1);
+    }
+    dist[sourceNode][0] = 0;
+
+    while(unmarkedVerticesRemain(dist)) {
+        minIndex = 0;
+        for(b = 0, min = Infinity; b < dist.length; b++){
+            if(dist[b][0] < min && dist[b][1] == -1) {
+                min = dist[b][0];
+                minIndex = b;
+            }
+        }
+        dist[minIndex][1] = dist[minIndex][0]
+
+        for(d = 0; d < graph[minIndex].length; d++) {
+            if(dist[minIndex][0] + graph[minIndex][d][1] < dist[graph[minIndex][d][0]][0])
+                dist[graph[minIndex][d][0]][0] = dist[minIndex][0] + graph[minIndex][d][1];
+        }
+    }
+    return dist;
+}
+
+function unmarkedVerticesRemain(dist) {
+    vertRemain = false;
+    for(c = 0; c < dist.length && vertRemain == false; c++){
+        if(dist[c][1] == -1)
+            vertRemain = true;
+    }
+    return vertRemain;
 }
